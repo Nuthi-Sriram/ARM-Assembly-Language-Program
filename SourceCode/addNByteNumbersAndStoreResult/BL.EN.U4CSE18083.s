605f159d9d56181f31769533469fe5430fb60291
Nuthi-Sriram <46497115+Nuthi-Sriram@users.noreply.github.com>
2020-06-10 12:33:42 +0530

    Changes to the code

--- SourceCode/addNByteNumbersAndStoreResult/BL.EN.U4CSE18083.s
+++ SourceCode/addNByteNumbersAndStoreResult/BL.EN.U4CSE18083.s
@@ -1,30 +1,24 @@
-;Write an ARM assembly language program to add N bytes numbers. Assume number of bytes stored in
-;memory location “n", source operands are stored in “src1” and “src2” memory locations. Store the result in
-;“result” memory location
-
 	AREA dat, DATA, READONLY
-n dcd 0x23, 0xDA, 0x44, 0x5A
-len dcd 4 
+src1 dcb 0xc0, 0x02, 0x3, 0x4
+src2 dcb 0x40, 0x06, 0x07, 0x08 
+n dcb 4
 	AREA res, DATA, READWRITE
-result dcd 0x00
-src1 dcd 0x00
-src2 dcd 0x00
+result dcb 0x00, 0x00, 0x00, 0x00
 	AREA pgm, CODE, READONLY
-	ldr r0, =n
-	ldr r1, =len
 	ldr r2, =result
 	ldr r3, =src1
 	ldr r4, =src2
-	ldr r1,[r1]
-loop ldr r3, [r0]
-	add r0,r0,#4
-	ldr r4, [r0]
-	add r6,r3,r4
-	add r7, r7, r6
-	str r7,[r2]
-	add r0,r0,#4
-	sub r1,r1,#2
-	cmp r1,#0
+	ldr r5, =n
+	ldr r6, [r5]
+	mov r8,#0
+loop 
+	ldrb r0,[r3],#1
+	ldrb r1,[r4],#1
+	add r0,r0, r8, LSR #8
+	add r8,	r0,r1
+	strb r8,[r2],#1
+	sub r6,r6,#1
+	cmp r6,#0
 	bne loop
 e b e
 	end
